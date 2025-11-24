fc73909f741de8b207f0714fbe2c0b47f072a890
goldshoot0720 <goldshoot0720@gmail.com>
2025-11-25 02:41:55 +0800

    TC40271186-20251125v37

--- FENGTUPRINFO-git.bat
+++ FENGTUPRINFO-git.bat
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "20251125v37-TC40271186-"
+git commit -m "TC40271186-20251125v37"
 git remote add origin https://github.com/fengtuprinfo/fengtuinfo.github.io.git
 git checkout -b main
 git push -u origin main --force
